4dd69e688f52b74117f77eb1d63e975ae9517523
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-26 17:48:37 -0400

    Add test code to operator function

--- script.js
+++ script.js
@@ -19,5 +19,12 @@ let divide = (a, b) => {
     return a / b;
 }
 let operate = (a, b) => {
-    
-}
+    let num1 = prompt("First number");
+    let number1 = parseInt(num1);
+    let opp = prompt("Operator");
+    let num2 = prompt("Second number");
+    let number2 = parseInt(num2);
+
+    console.log(number1 + number2)
+}  
+operate();
